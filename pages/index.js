11f1de0345e59d1c97f080844afc213466df1d05
Daniel Vitor Pereira da Silva <danielvitor081@gmail.com>
2021-07-19 19:19:10 -0300

    Aula 2

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,39 @@
+import React from 'react';
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
-import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
+import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
 
 function ProfileSidebar(propriedades) {
   return (
     <Box>
       <img src={`http://github.com/${propriedades.githubUser}.png`} style={{ borderRadius: '8px' }} />
+      <hr />
+
+      <p>
+        <a className="boxLink" href={`https://github.com/${propriedades.githubUser}`}>
+          @{propriedades.githubUser}
+        </a>
+      </p>
+
+      <hr />
+
+      <AlurakutProfileSidebarMenuDefault></AlurakutProfileSidebarMenuDefault>
     </Box>
   )
 }
 
 export default function Home() {
   const githubUser = 'danielvito-pereira';
+  const [comunidades, setComunidades] = React.useState([{
+    id:'135135135151351',
+    title:'Eu odeio acordar cedo',
+    image:''
+  }
+  ]);
+   /*const comunidades = comunidades [0];
+    const alteradorDeComunidades = comunidades [1];
+    const comunidades = ['Alurakut'];*/
   const pessoasFavoritas = [
     'juunegreiros',
     'omariosouto',
@@ -41,9 +62,62 @@ export default function Home() {
 
             <OrkutNostalgicIconSet />
           </Box>
+
+
+          <Box>
+            <h2 className="subTitle">O que vicê deseja fazer?</h2>
+            <form onSubmit={function handleCriaComunidade(e) {
+              e.preventDefault();
+              const dadosDoForm = new FormData(e.target);
+              
+
+              const comunidade = {
+                id: new Date().toISOString(),
+                title: dadosDoForm.get('title'),
+                image: dadosDoForm.get('image'),
+
+              }
+              const comunidadesAtualizadas = [...comunidades, comunidade]
+              setcomunidades(comunidadesAtualizadas)
+            }}>
+              <div>
+                <input
+                placeholder="Qual vai ser o nome da sua comunidade?" 
+                name="title" 
+                aria-label="Qual vai ser o nome da sua comunidade?"
+                type="text"
+                />
+              </div>
+
+              <div>
+                <input
+                placeholder="Coloque uma URL para usarmos de capa" 
+                name="image" 
+                aria-label="Coloque uma URL para usarmos de capa"/>
+              </div>
+
+              <button>
+                    Criar comunidade
+              </button>
+            </form>
+          </Box>
         </div>
 
         <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+        <ProfileRelationsBoxWrapper>
+              <ul>
+                {comunidades.map((itemAtual) => {
+                  return (
+                    <li key={itemAtual.id}>
+                      <a href={`/user/${itemAtual.title}`}>
+                        <img src={itemAtual.image} />
+                        <span>{itemAtual.title}</span>
+                      </a>
+                    </li>
+                  )
+                })}
+              </ul>
+        </ProfileRelationsBoxWrapper>
           <ProfileRelationsBoxWrapper>
             <h2 className="smallTitle">
               Pessoas da comunidade ({pessoasFavoritas.length})
@@ -52,8 +126,8 @@ export default function Home() {
             <ul>
               {pessoasFavoritas.map((itemAtual) => {
                 return (
-                  <li>
-                    <a href={`/user/${itemAtual}`} key={itemAtual}>
+                  <li key={itemAtual}>
+                    <a href={`/user/${itemAtual}`} >
                       <img src={`http://github.com/${itemAtual}.png`} />
                       <span>{itemAtual}</span>
                     </a>
